a1c74ae0d05330cdff7b70ef51451374ee5714e4
marcus-rubcube <marcus.vinicius@rubcube.com>
2024-10-20 10:34:12 -0300

    feat: test grafo_2.txt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Matriz matriz = Arquivo.converterParaMatriz("src/grafo.txt");
+        Matriz matriz = Arquivo.converterParaMatriz("src/grafo_2.txt");
         System.out.printf("Árvore geradora mínima:\n%s", matriz.primAlgorithm());
     }
 }
